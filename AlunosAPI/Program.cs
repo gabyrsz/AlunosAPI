5482e3a1fedadf9b292523006a83c39401101f35
gabyrsz <gabriellerossi.ifmg@gmail.com>
2024-09-05 21:47:42 -0300

    Add files via upload

---
+++ AlunosAPI/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crud Pessoa V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
